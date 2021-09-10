135c602a06b0ec72a28c38a37796472835bae947
NotKashish <kashishmaru2001@gmail.com>
2021-09-10 20:13:03 +0530

    Different login for Farmers and wholesalers

--- lib/screens/authentication/authentication_wrapper.dart
+++ lib/screens/authentication/authentication_wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fasal/screens/authentication/signin.dart';
 import 'package:fasal/screens/farmers/farmers_homepage.dart';
+import 'package:fasal/screens/wholesalers/wholesalers_homepage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -20,9 +21,10 @@ class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
       String? userType;
       CollectionReference users =
       FirebaseFirestore.instance.collection('users');
-       users
+       var user = users
           .doc(firebaseUser.uid)
           .get();
+
        //Todo: Idk why it's not working, maybe it'll work for you, who knows flutter is annoying
     }
   }
@@ -38,9 +40,27 @@ class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
   Widget build(BuildContext context) {
 
     final firebaseUser = context.watch<User?>();
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
 
     if(firebaseUser != null){
-      return FarmersHomepage();
+      return FutureBuilder<DocumentSnapshot>(
+        future: users.doc(firebaseUser.uid).get(),
+          builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.hasError)
+              return Text("Something went wrong");
+            if (snapshot.hasData && !snapshot.data!.exists)
+              return Text("Document Does not Exist");
+            if (snapshot.connectionState == ConnectionState.done) {
+              Map<String, dynamic> data = snapshot.data!.data() as Map<
+                  String,
+                  dynamic>;
+              if (data['type'] == 'farmer')
+                return FarmersHomepage();
+              if (data['type'] == 'wholesaler')
+                return WholesalerHomepage();
+            }
+            return Text("Loading");
+          });
     }
     return SignIn();
   }
--- lib/screens/wholesalers/wholesalers_homepage.dart
+++ lib/screens/wholesalers/wholesalers_homepage.dart
@@ -2,8 +2,8 @@ import 'package:fasal/services/authentication_services.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class FarmersHomepage extends StatelessWidget {
-  const FarmersHomepage({Key? key}) : super(key: key);
+class WholesalerHomepage extends StatelessWidget {
+  const WholesalerHomepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +11,10 @@ class FarmersHomepage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Hey Milloni, with double L, you are a farmer"),
+            Text("Hey Mit, you are a wholesaler, I would add something funny "
+                "but nothing is coming to mind"),
             ElevatedButton(onPressed: () {
               context.read<AuthenticationService>().signOut();
             },
