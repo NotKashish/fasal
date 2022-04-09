83539d712a8187cdb3cce683f96357ddba970939
mit <mitsheth008@gmail.com>
2022-04-09 15:08:03 +0530

    ui form

--- lib/screens/farmers/form_page.dart
+++ lib/screens/farmers/form_page.dart
@@ -29,7 +29,7 @@ class _FormPageState extends State<FormPage> {
         ? buildCompleted()
       : Theme(
         data: Theme.of(context).copyWith(
-          colorScheme: ColorScheme.light(primary: androidGreen,),
+          colorScheme: ColorScheme.light(primary: oliveGreen,),
         ),
         child: Stepper(
           //type: StepperType.horizontal,
@@ -89,7 +89,67 @@ class _FormPageState extends State<FormPage> {
       state: currentStep > 0 ? StepState.complete : StepState.indexed,
       isActive: currentStep >= 0,
       title: Text('One'),
-      content: Container(),
+      content: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('State:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child:
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    suffixIcon: Icon(Icons.house),
+                    hintText: 'Enter your State',
+                    hintStyle: TextStyle(color: Colors.black87,),
+                    contentPadding: EdgeInsets.all(15.0),
+                  ),
+                ),
+              // TextField(
+              //
+              //   decoration: InputDecoration(
+              //     border: InputBorder.none,
+              //     suffixIcon: Icon(Icons.house),
+              //     hintText: 'Enter Your State',
+              //     hintStyle: TextStyle(color: white,),
+              //     contentPadding: EdgeInsets.all(15.0),
+              //   ),
+              // ),
+            ),
+            const SizedBox(height: 20.0,
+            ),
+            Text('District:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.other_houses),
+                  hintText: 'Enter Your District',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
 
     ),
     Step(
@@ -97,7 +157,78 @@ class _FormPageState extends State<FormPage> {
       isActive: currentStep >= 1,
 
       title: Text('Two'),
-      content: Container(),
+      content: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Temperature:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.local_fire_department),
+                  hintText: 'Enter the Temperature',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20.0,
+            ),
+            Text('Humidity:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.invert_colors_on),
+                  hintText: 'Enter the Humidity',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 15,
+            ),
+            Text('Rainfall:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.water),
+                  hintText: 'Enter the Rainfall',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
 
     ),
     Step(
@@ -105,8 +236,179 @@ class _FormPageState extends State<FormPage> {
       isActive: currentStep >= 2,
 
       title: Text('Three'),
-      content: Container(),
+      content: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('N:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.invert_colors_sharp),
+                  hintText: 'Enter the Nitrogen Level',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20.0,
+            ),
+            Text('P:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.invert_colors_sharp),
+                  hintText: 'Enter the Phosphorous Level',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 15,
+            ),
+            Text('K:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.invert_colors_sharp),
+                  hintText: 'Enter the Potassium Level',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Text('pH:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.invert_colors_sharp),
+                  hintText: 'Enter the pH',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
 
+          ],
+        ),
+      ),
+
+    ),
+    Step(
+      state: currentStep > 3 ? StepState.complete : StepState.indexed,
+      isActive: currentStep >= 3,
+
+      title: Text('Four'),
+      content: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Budget:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.money),
+                  hintText: 'Enter your Budget',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20.0,
+            ),
+            Text('Area:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.area_chart),
+                  hintText: 'Enter your Area',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 15,
+            ),
+            Text('Other Details:-',
+              style: TextStyle(fontSize: 18,),
+            ),
+            const SizedBox(height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: androidGreen,
+                borderRadius:  BorderRadius.circular(25),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.details),
+                  hintText: 'Enter the Other Details',
+                  hintStyle: TextStyle(color: Colors.black87,),
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
     ),
   ];
 
